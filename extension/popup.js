6a98fd801f5febcccc12363bd4f8a97a45e88f80
Shkyyl <szymonpiasecki8@gmail.com>
2023-09-09 12:59:07 +0200

    Version 1.4
    Fix extension to work for new keydrop layout,
    Note: extension sometimes joins to 100,1000 ticket battles for now i leave it, maybe in future update to join only for 1 ticket battles

--- extension/popup.js
+++ extension/popup.js
@@ -1,26 +1,26 @@
-function joinCaseBattle(){
-
-  chrome.storage.local.get(["TARGETS"], function(result){
+function joinCaseBattle() {
+  chrome.storage.local.get(["TARGETS"], function(result) {
     const targets = result.TARGETS || ['TOXIC', 'DIABLO', 'SPARK', 'ICE BLAST', 'TEETH', 'BEAST', 'SERENITY', 'JOY', 'PROGRESS', 'KITTY', 'ONYX'];
     (async () => {
       while (true) {
         await new Promise(r => setTimeout(r, 100));
         try {
-          const caseNames = (([...document.querySelectorAll('p.max-w-full.px-1.overflow-hidden')]).splice(0, 10)).map(e => e.textContent);
-          const casePrices = (([...document.querySelectorAll('div.flex.items-center.justify-center.rounded-tl-lg')]).splice(0, 10)).map(e => e.textContent);
-          if (casePrices[caseNames.indexOf(caseNames.find(name => targets.includes(name)))] === 'FREE' && caseNames.some(str => targets.includes(str))) {
+          const caseNames = [...document.querySelectorAll('p.max-w-full.px-1.overflow-hidden')].splice(0, 5).map(e => e.textContent);
+          const casePrices = [...document.querySelectorAll('div.w-fit p.text-center.font-semibold.leading-none')].splice(0, 5).map(e => e.textContent);
+          const targetIndex = caseNames.findIndex(name => targets.includes(name));
+          if (casePrices[targetIndex] === 'x1' && targetIndex !== -1) {
             const btns = [...document.querySelectorAll('a.button.mr-5')];
-            const btn = btns[caseNames.indexOf(caseNames.find(name => targets.includes(name)))];
+            const btn = btns[targetIndex];
             btn.click();
-          }         
+          }
         } catch {null;}
       }
     })();
-  })
+  });
 }
 
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById("join-button").addEventListener('click', function(){
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("join-button").addEventListener('click', function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       if (tabs.length > 0) {
         var tabId = tabs[0].id;
@@ -32,8 +32,8 @@ document.addEventListener("DOMContentLoaded", function(){
         console.error("No active tab found.");
       }
     });
-  })
-  document.getElementById("options-button").addEventListener('click', function(){
+  });
+  document.getElementById("options-button").addEventListener('click', function() {
     chrome.runtime.openOptionsPage();
-  })
-})
+  });
+});
